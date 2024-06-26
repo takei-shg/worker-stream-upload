47cb821a6a966f5b97437739e94bd850c17ef041
takei-shg <takei.shg@gmail.com>
2024-06-26 15:32:59 -0700

    add biome and apply

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,18 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
 
 type Bindings = {
-   STORAGE_DEV1_BUCKET: R2Bucket
-}
+  STORAGE_DEV1_BUCKET: R2Bucket;
+};
 
+const app = new Hono<{ Bindings: Bindings }>();
 
-const app = new Hono<{ Bindings: Bindings }>()
+app.post("/upload", (c) => {
+  const fileUrl = c.req.query("fileUrl");
+  const now = new Date();
+  const timestamp = now.toISOString().replace(/[-:T]/g, "").slice(2, 12);
+  const fileName = `${timestamp}.mp3`;
 
-app.post('/upload', (c) => {
-  const fileUrl = c.req.query('fileUrl')
+  return c.text("Hello Hono!");
+});
 
-  return c.text('Hello Hono!')
-})
-
-export default app
+export default app;
