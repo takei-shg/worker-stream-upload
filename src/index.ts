55a8238166d5d03a2ec168bc9ca1354865884f6d
takei-shg <takei.shg@gmail.com>
2024-06-28 16:20:05 -0700

    add stream upload

--- src/index.ts
+++ src/index.ts
@@ -6,13 +6,41 @@ type Bindings = {
 
 const app = new Hono<{ Bindings: Bindings }>();
 
-app.post("/upload", (c) => {
-  const fileUrl = c.req.query("fileUrl");
-  const now = new Date();
-  const timestamp = now.toISOString().replace(/[-:T]/g, "").slice(2, 12);
-  const fileName = `${timestamp}.mp3`;
+app.post("/upload", async (c) => {
+  const body = await c.req.json();
+  const fileUrl = body.fileUrl;
 
-  return c.text("Hello Hono!");
+  if (!fileUrl) {
+    return c.text('Missing MP3 URL', 400)
+  }
+
+  const directory = 'gd'
+  const url = new URL(fileUrl);
+  const fileName = `${directory}/${url.pathname.split('/').pop()}`;
+  if (!fileName) {
+    return c.text('Invalid file URL', 400);
+  }
+
+  const response = await fetch(fileUrl);
+  if (!response.ok) {
+    throw new Error(`Failed to fetch file: ${response.statusText}`);
+  }
+
+  try {
+    const uploadResult = await c.env.STORAGE_DEV1_BUCKET.put(fileName, response.body, {
+      httpMetadata: {
+        contentType: 'audio/mpeg',
+      },
+    })
+    if (!uploadResult.uploaded) {
+      throw new Error('Failed to upload file');
+    }
+  } catch (error) {
+    console.error('Error uploading file', error);
+    throw new Error('Failed to upload file');
+  }
+
+  return c.json({ message: "Upload done.", fileName: `${fileName}` });
 });
 
 export default app;
